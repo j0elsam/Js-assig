598a9c9c99234bf302ce2f2daa06a6dd39316b60
joelsam <joelspalliyamvila@gmail.com>
2024-11-13 15:29:22 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,24 @@
+const a = document.getElementById("main-title");
+console.log(a);
+
+const containers = document.getElementsByClassName('container');
+console.log(containers);
+
+const sections = document.getElementsByClassName('section');
+console.log(sections);
+
+const p = document.getElementsByTagName('p');
+console.log(p);
+
+
+const firstButton = document.querySelector('.btn');
+console.log(firstButton);
+
+const firstSection = document.querySelector('.section');
+console.log(firstSection);
+
+const allButtons = document.querySelectorAll('.btn');
+console.log(allButtons);
+
+const paragraphsInContainer = document.querySelectorAll('.container p');
+console.log(paragraphsInContainer);
